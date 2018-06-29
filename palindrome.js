45c965b16cc9a2c55e6a0cda6bbf896ac15e2ccd
prakarsh shrivastava <prakar.vastava@gmail.com>
2018-06-29 10:47:08 +0530

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,5 +1,6 @@
 function palindrome (num)
 {
+var num,temp,a,b=0;
 temp=num;
 while(temp>0)
 {
@@ -7,7 +8,7 @@ while(temp>0)
   b=(10*b)+a;
   temp=parseInt(temp/10);
 }
-if(temp===num)
+if(b===num)
 {
   return 1;
 }
@@ -16,15 +17,16 @@ else
   return 0;
 }
 }
-var num,num1,temp,a,b=0,i,j,mul;
+var i,j,mul,ans=0;
 for(i=999;i>99;i--)
 {
   for(j=999;j>99;j--)
   {
     mul=i*j;
-    if(palindrome(mul)===1)
+    if(palindrome(mul)===1 && mul>ans )
     {
-      console.log(mul);
+      ans=mul;
     }
   }
 }
+console.log(ans);
